ef470b070e6cfc39f2043762d722662f183440c7
belovetech <belovetech@gmail.com>
2022-10-19 22:24:23 +0100

    [Feat]: implement HTTP methods with express

---
+++ natours/app.js
@@ -0,0 +1,98 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const express = require('express');
+
+// Create express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// serialize json  data to js object
+const dirname = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(dirname));
+
+// GET endpoints
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const tour = tours.find(cur => cur.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// POST endpoints
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(dirname, JSON.stringify(tours), err => {
+    if (err) return console.log(err);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newTour,
+      },
+    });
+  });
+});
+
+// PATCH endpoint
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const tourIndex = tours.findIndex(cur => cur.id === id);
+  const tour = tours[tourIndex];
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const updateTour = Object.assign(tour, req.body);
+  tours.splice(tourIndex, 1, updateTour);
+
+  fs.writeFile(dirname, JSON.stringify(tours), err => {
+    if (err) return console.log(err);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updateTour,
+      },
+    });
+  });
+});
+
+// setting up server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
