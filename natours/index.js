d1e7b8a58d919ca18fb6c37c6cfb455dd64a2eeb
belovetech <belovetech@gmail.com>
2022-10-20 09:19:11 +0100

    [Update]: test file

--- natours/index.js
+++ natours/index.js
@@ -115,6 +115,25 @@ app.delete('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+/*
+ROUTES
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+*/
+/*
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+  
+*/
+
 // setting up server
 const port = 3000;
 app.listen(port, () => {
