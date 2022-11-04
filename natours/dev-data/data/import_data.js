80c150be020e52aa4a284d78b34a7df20d14d43b
belovetech <belovetech@gmail.com>
2022-11-04 16:08:07 +0100

    [Feat]: add import and delete data script

---
+++ natours/dev-data/data/import_data.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+const Tour = require("../../models/tourModel");
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const mongoose = require("mongoose");
+
+// Connect Database
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data successfully loaded!");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted!");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+} else {
+  console.log(
+    `Flag is missing!!!
+\tinclude --import to load data
+\tinclude --delete to empty data`
+  );
+
+  process.exit();
+}
