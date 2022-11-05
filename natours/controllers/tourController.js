7544e6c17f6d6ccafde526f5c47d122b17879f2b
belovetech <belovetech@gmail.com>
2022-11-05 17:44:07 +0100

    [Feat]: add utils directory

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIfeatures = require("./../utils/apiFeatures");
 
 // TOUR ROUTER MIDDLEWARE
 exports.aliasTopTours = (req, res, next) => {
@@ -7,61 +8,6 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = "name,price,summary,ratingsAverage,difficulty,";
   next();
 };
-
-class APIfeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    // SIMPLE FILTERING
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    // SORTING
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
-    return this;
-  }
-
-  limitFields() {
-    // FIELDS LIMITING
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(",").join(" ");
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select("-__v");
-    }
-    return this;
-  }
-
-  paginate() {
-    // PAGINATION
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 // ROUTES HANDLERS
 exports.getAllTours = async (req, res) => {
