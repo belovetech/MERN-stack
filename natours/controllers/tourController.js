42caaa644fe3e1120edca3931a96221df0a622d7
belovetech <belovetech@gmail.com>
2022-11-05 19:38:38 +0100

    [Feat]: implement get tour monthly plan

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -146,3 +146,52 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPLan = async (req, res) => {
+  const year = req.params.year * 1;
+  const plans = await Tour.aggregate([
+    {
+      $unwind: "$startDates",
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $month: "$startDates" },
+        numTourStart: { $sum: 1 },
+        tours: { $push: "$name" },
+      },
+    },
+    {
+      $addFields: { month: "$_id" },
+    },
+    {
+      $project: { _id: 0 },
+    },
+    {
+      $sort: { numTourStart: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
+  try {
+    res.status(200).json({
+      status: "success",
+      data: {
+        plans,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Resources Not Found",
+    });
+  }
+};
