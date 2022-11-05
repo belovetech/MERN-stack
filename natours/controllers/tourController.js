de791995db88f97e9dcd1b25b2d6fbdc92fc5d2a
belovetech <belovetech@gmail.com>
2022-11-05 12:06:22 +0100

    [Update]: add more features

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -3,7 +3,24 @@ const Tour = require("../models/tourModel");
 // ROUTES HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query =  Tour.find({ difficulty: "easy", duration: "5" });
+    // const query =  Tour.find()
+    //   .where("difficulty")
+    //   .equals("easy")
+    //   .where("duration")
+    //   .equals(5);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,
--- natours/dev-data/data/import_data.js
+++ natours/dev-data/data/import_data.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 const fs = require("fs");
 
 const Tour = require("../../models/tourModel");
