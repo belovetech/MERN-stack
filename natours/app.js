c8bca1d70031d20fa2aa595ae593f44fd66fc038
belovetech <belovetech@gmail.com>
2022-11-06 19:49:01 +0100

    [Feat]: implement custom error class

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 
@@ -31,25 +33,13 @@ app.use("/api/v1/users", userRouter);
 
 // Handle Unhandle Routes
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = "fail";
-  err.statusCode = 400;
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = "fail";
+  // err.statusCode = 400;
+  next(new AppError(`Can't find ${req.originalUrl} on this server`), 400);
 });
 
 // ERROR MIDDLEWARE
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "Error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
